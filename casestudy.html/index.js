588de4d60d3aec2dfa6388f1d2394ad2d26ca647
Nguyen To Tuan Hiep <tuanhiep21112001@gmail.com>
2024-04-08 20:31:07 +0700

    codegym

--- casestudy.html/index.js
+++ casestudy.html/index.js
@@ -6,3 +6,37 @@ const adressclose = document.querySelector('#address-form-close')
 adressclose.addEventListener("click", function(){
     document.querySelector('.address-form').style.display = "none"
 })
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
